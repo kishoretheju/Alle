6bcfd07beb8cccccb243298b65d7091a78c876b2
Kishore Thejasvi M <kishore@apples-MacBook-Pro.local>
2023-09-30 11:42:05 +0530

    Getting ocr texts from image

--- Alle/Models/Image.swift
+++ Alle/Models/Image.swift
@@ -6,13 +6,54 @@
 //
 
 import Foundation
+import UIKit
+import VisionKit
+import Vision
 
 class Image {
   let name: String
   let creationDate: Date = Date()
+  var ocrTexts: [String]? = nil
+  
+  typealias CompletionBlock = (_ ocrTexts: [String]? ,_ error: Error?) -> ()
   
   init(name: String) {
     self.name = name
+  }
+  
+  func getOcrDescription(withCompletion completion: @escaping CompletionBlock) {
+    if let unwrappedOcrTexts = ocrTexts {
+      return completion(ocrTexts, nil)
+    }
+    
+    guard let cgImage = UIImage(named: name)?.cgImage
+    else { return }
+    
+    let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+    let request = VNRecognizeTextRequest{ [weak self] request, error in
+      guard let observations = request.results
+      else {
+        return
+      }
+      
+      let recognizedStrings: [String] = observations.compactMap { observation in
+        guard let textObservation = observation as? VNRecognizedTextObservation
+        else
+        { return nil }
+        
+        return textObservation.topCandidates(1).first?.string
+      }
+      
+      self?.ocrTexts = recognizedStrings
+      completion(recognizedStrings, nil)
+    }
+    
+    do {
+      try requestHandler.perform([request])
+    }
+    catch {
+      completion(nil, error)
+    }
   }
 }
 
